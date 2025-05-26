15d3d9edc91e5a0ab6a1a781f21c69dd129292b7
krupa625 <krupapatel1574@gmail.com>
2025-05-26 10:59:03 +0530

    debug

--- src/config/db.js
+++ src/config/db.js
@@ -5,6 +5,7 @@ const connectDb = async () => {
   try {
     await mongoose.connect(mongo_url);
     console.log("mongodb connected!");
+    mongoose.set("debug", true);
   } catch (err) {
     console.log("Error connecting db", err);
   }
